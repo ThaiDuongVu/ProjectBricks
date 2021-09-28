79e64b3053da3b5f647087139de2e2cf6499f83d
Duong Vu <vu.t.duong01@gmail.com>
2021-09-27 20:40:29 -0400

    Fix max score bug

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -37,7 +37,7 @@ public class GameController : MonoBehaviour
         get => score;
         set
         {
-            if (value < MaxScore)
+            if (value > MaxScore)
             {
                 score = MaxScore;
                 return;
